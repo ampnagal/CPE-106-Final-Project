94465337d628a57185a613a04c3b3eea355a6c54
ampnagal <163994417+ampnagal@users.noreply.github.com>
2025-03-21 09:45:59 +0800

    Update Voting.py

--- Voting.py
+++ Voting.py
@@ -137,4 +137,5 @@ def results():
     return render_template("results.html", votes=votes)
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
